5b6727b89b7ea1384af0a4069b66d9dfe7302810
Saul Alonso <isaul37@hotmail.es>
2016-11-12 16:15:12 -0600

    se corrigio formato de lists/test.py

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
-class SmokeTests(TestCase):
 
+class SmokeTests(TestCase):
     def test_bad_maths(self):
         self.assertEqual(1 + 1, 3)
