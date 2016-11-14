5a87f7a003b6e0658bc9413a7e44a163bbf0311a
Saul Alonso <isaul37@hotmail.es>
2016-11-14 00:45:25 -0600

    run flake8

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -2,15 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
-
     def tearDown(self):
         self.browser.quit()
-
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
