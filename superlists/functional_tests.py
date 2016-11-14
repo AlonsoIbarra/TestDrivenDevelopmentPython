4ad534b2fb342f29f56074053c67dd95dac540d6
Saul Alonso <isaul37@hotmail.es>
2016-11-14 00:16:20 -0600

    se refactorizó home.html

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,18 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
-
-
-class NewVisitorsTest(unittest.TestCase):
+class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
+
     def tearDown(self):
         self.browser.quit()
 
+
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://127.0.0.1:8000')
+        self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
@@ -26,7 +26,7 @@ class NewVisitorsTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock fathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows)
         )
         self.fail('Finish the test!')
 
