2624b5e8b2f33d8265607b14bad6bc9c993a9258
AlonsoIbarra <saul.ibarra@cimat.mx>
2016-11-13 22:52:35 -0600

    Update functional_tests.py

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -12,7 +12,7 @@ class NewVisitorsTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get('http://127.0.0.1:8000')
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
