7cd8cec744608d6f9ba910d5d47012389a7ef85e
Saul Alonso <isaul37@hotmail.es>
2016-11-14 23:48:58 -0600

    some defects was fixed

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -46,7 +46,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url. 'lists/.+')
+        self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNoEqual(francis_list_url, edit_list_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -66,7 +66,7 @@ class ListViewTest(TestCase):
         other_list = List.objects.create()
         correct_list = List.objects.create()
         response = self.client.get('/lists/%d/' % (correct_list.id,))
-        assertEqual(response.context['list'], correct_list)        
+        self.assertEqual(response.context['list'], correct_list)        
 
 
 class NewListTest(TestCase):
