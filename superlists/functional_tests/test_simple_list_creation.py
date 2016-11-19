dafd925c5e68193006a9943b3a3243aacd130233
Saul Alonso <isaul37@hotmail.es>
2016-11-19 15:42:28 -0600

    rename all item input ids and names. still broken

--- superlists/functional_tests/test_simple_list_creation.py
+++ superlists/functional_tests/test_simple_list_creation.py
@@ -9,7 +9,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -19,7 +19,7 @@ class NewVisitorTest(FunctionalTest):
         edit_list_url = self.browser.current_url
         self.assertRegex(edit_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -30,7 +30,7 @@ class NewVisitorTest(FunctionalTest):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         francis_list_url = self.browser.current_url
